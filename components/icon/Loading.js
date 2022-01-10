054550fe71f0d676f1b2b94367227ac259f80953
Khoironi Kurnia Syah <khoironidev@gmail.com>
2022-01-10 20:08:09 +0800

    feat: add loading comp

---
+++ components/icon/Loading.js
@@ -0,0 +1,18 @@
+export default function Loading() {
+  return (
+    <>
+      <div className="flex items-center justify-center animate-spin">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          width={24}
+          height={24}
+          style={{
+            fill: "#4895ef",
+          }}
+        >
+          <path d="M12 22c5.421 0 10-4.579 10-10h-2c0 4.337-3.663 8-8 8s-8-3.663-8-8c0-4.336 3.663-8 8-8V2C6.579 2 2 6.58 2 12c0 5.421 4.579 10 10 10z" />
+        </svg>
+      </div>
+    </>
+  );
+}
